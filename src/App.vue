ae96e27e9a9d7ee2e13f61e56d1acf0e2828b85e
MyungJunChae <cmj9597@naver.com>
2018-10-03 09:11:04 +0900

    Add UI, home component

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,12 @@ export default {
 </script>
 
 <style>
+@import url(http://fonts.googleapis.com/earlyaccess/notosanskr.css);
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        
+    <div id="home_wrapper">
+        <div>
+            <p id="main_text">D.com Finance</p>
+        </div>
+        <div id="navigator">
+            <div>
+                <div id="mark"></div>
+                <div id="link">
+                    <router-link :to="{ name:'usersList'}">UserList</router-link>
+                    <router-link :to="{ name:''}">Finance</router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -9,5 +20,55 @@ export default {
 }
 </script>
 <style scoped>
-
+#home_wrapper{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 100vh;
+    background-image: url("../assets/home-background.png");
+}
+#home_wrapper > div:nth-child(1){
+    display: flex;
+    align-items: center;
+}
+#main_text{
+    margin: 0px 0px 100px 100px;
+    font-size: 80px;
+    font-weight: 500;
+    color:#FFFFFF;
+}
+#navigator{
+    display: flex;
+    align-items: center;
+    width:25%;
+    background-color:#5d5d5d;
+    opacity: 0.95;
+}
+#navigator > div{
+    margin-left: 60px;
+    width: 40%;
+}
+#navigator > div > p{
+    color:#FFFFFF;
+}
+#mark{
+    width: 0px;
+    height: 0px;
+    border: 15px solid #ffffff;
+    
+    border-right-color:RGB(0,0,0,0);
+    border-bottom-color:RGB(0,0,0,0);
+    opacity: 0.95;
+}
+#link{
+    display: flex;
+    flex-direction: column;
+}
+#link > *{
+    margin-left:35px;
+    text-decoration: none;
+    color:#ffffff;
+    font-size: 25px;
+    opacity: 0.95;
+}
 </style>
