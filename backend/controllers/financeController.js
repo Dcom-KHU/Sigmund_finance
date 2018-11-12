aa7f599a55ba790d2a925e11413f9891015e140b
kairos03 <kairos9603@gmail.com>
2018-11-13 00:53:46 +0900

    add finance controller skeleton code

---
+++ backend/controllers/financeController.js
@@ -0,0 +1,33 @@
+var Finance = require('../model/Finance');
+
+exports.finance_list = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: finance list');
+};
+
+exports.finance_detail = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: finance detail');
+};
+
+exports.finance_create_get = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: finance create get');
+};
+
+exports.finance_create_post = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: finance create post');
+};
+
+exports.finance_delete_get = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: finance delete get');
+};
+
+exports.finance_delete_post = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: finance delete post');
+};
+
+exports.finance_update_get = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: finance update get');
+};
+
+exports.finance_update_post = function(req, res, next) {
+    res.send('NOT IMPLEMENTED: finance update post');
+};
