a1b9921e20c7b8ae3c234f8a6bfec9967cc4a3fc
kairos03 <kairos9603@gmail.com>
2018-11-13 01:01:34 +0900

    add user controller skeleton code

---
+++ backend/controllers/userController.js
@@ -0,0 +1,33 @@
+var User = require('../model/User');
+
+exports.user_list = function(res, req, next){
+    res.send('NOT IMPLEMENTED: user list');
+};
+
+exports.user_detail = function(res, req, next){
+    res.send('NOT IMPLEMENTED: user detail');
+};
+
+exports.user_create_get = function(res, req, next){
+    res.send('NOT IMPLEMENTED: user create get');
+};
+
+exports.user_create_post = function(res, req, next){
+    res.send('NOT IMPLEMENTED: user create post');
+};
+
+exports.user_delete_get = function(res, req, next){
+    res.send('NOT IMPLEMENTED: user delete get');
+};
+
+exports.user_delete_post = function(res, req, next){
+    res.send('NOT IMPLEMENTED: user delete post');
+};
+
+exports.user_update_get = function(res, req, next){
+    res.send('NOT IMPLEMENTED: user update get');
+};
+
+exports.user_update_post = function(res, req, next){
+    res.send('NOT IMPLEMENTED: user update post');
+};
